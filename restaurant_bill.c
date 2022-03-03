855ddd6398bb7d8fb576f2ea3b912e0147611219
Policarpio <gabpolicarpio485@student.citruscollege.edu>
2022-03-02 20:40:27 -0800

    add comments

--- restaurant_bill.c
+++ restaurant_bill.c
@@ -11,12 +11,14 @@
 
 int main ()
 {
+    // Picks randomly between 4 different meals
     srand(time(NULL));
 
     int meal = rand()% 4 + 1;
     double taxPercentage, tipPercentage;
     double mealCost, tax, tip, total;
 
+    // Asks user for input regarding tax and tip value in %
     printf("Enter tax as percentage: ");
     scanf("%lf", &taxPercentage);
 
@@ -25,45 +27,49 @@ int main ()
 
     printf("\n");
 
+    // If else statements that goes through all possible meal options and randomly
+    // picks between them and also states each meal's cost
     if(meal == 1)
     {
         printf("Meal Ordered: Salad\n");
         printf("\n");
-        printf("Salad Cost        = $         9.95\n");
+        printf("Salad Cost           = $         9.95\n");
         mealCost = 9.95;
     }
     else if(meal == 2)
     {
         printf("Meal Ordered: Soup\n");
         printf("\n");
-        printf("Soup Cost         = $         4.55\n");
+        printf("Soup Cost            = $         4.55\n");
         mealCost = 4.55;
     }
     else if(meal == 3)
     {
         printf("Meal Ordered: Sandwich\n");
         printf("\n");
-        printf("Sandwich Cost     = $        13.25\n");
+        printf("Sandwich Cost        = $        13.25\n");
         mealCost = 13.25;
     }
     else
     {
         printf("Meal Ordered: Pizza\n");
         printf("\n");
-        printf("Pizza Cost        = $        22.35\n");
+        printf("Pizza Cost           = $        22.35\n");
         mealCost = 22.35;
     }
 
+    // Equations that calculate both tax and tip costs and adds the total
     tax = mealCost * taxPercentage / 100;
     tip = mealCost * tipPercentage / 100;
 
     total = mealCost + tax + tip;
 
-    printf("Total Tax  (%.1lf%%) = $ %12.2lf\n",taxPercentage,tax);
-    printf("Tip Amount (%.1lf%%) = $ %12.2lf\n",tipPercentage,tip);
-    printf("------------------------------------\n");
-    printf("Total Cost        = $ %12.2lf\n",total);
-    printf("------------------------------------\n");
+    // Prints out a sumation of all the costs in an orderly format
+    printf("Total Tax  (%.1lf%%)   = $ %12.2lf\n",taxPercentage,tax);
+    printf("Tip Amount (%.1lf%%)   = $ %12.2lf\n",tipPercentage,tip);
+    printf("-------------------------------------\n");
+    printf("Total Cost           = $ %12.2lf\n",total);
+    printf("-------------------------------------\n");
 
     return 0;
 }
